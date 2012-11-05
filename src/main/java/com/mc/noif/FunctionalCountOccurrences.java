72dbef726f76a887d2d5060863e47e3f52a7c31c
Michael Case <michael_case@apple.com>
2012-11-05 14:57:18 -0800

    generified Function interface

--- src/main/java/com/mc/noif/FunctionalCountOccurrences.java
+++ src/main/java/com/mc/noif/FunctionalCountOccurrences.java
@@ -2,8 +2,8 @@ package com.mc.noif;
 
 public class FunctionalCountOccurrences implements CountOccurrences {
     
-    Function zero = new Scalar(0);
-    Function negativeOne = new Scalar(-1);
+    Constant<Integer> zero = new Constant(0);
+    Constant<Integer> negativeOne = new Constant(-1);
     
     @Override
     public int find(int target, int[] arr) {
@@ -13,7 +13,7 @@ public class FunctionalCountOccurrences implements CountOccurrences {
         );
     }
     
-    private class TheAlgorithm implements Function {
+    private class TheAlgorithm implements Function<Integer> {
         int target;
         int[] arr;
         
@@ -21,12 +21,12 @@ public class FunctionalCountOccurrences implements CountOccurrences {
             this.target = target;
             this.arr = arr;
         }
-        public Object execute() {
+        public Integer execute() {
             int left = findLeft(target, arr);
             int right = findRight(target, arr);
             return B.ool(left == -1 && right == -1).branch(
                 zero,
-                new Result(Range.size(left, right))
+                new Range(left, right).size
             );
         }
     }
@@ -34,17 +34,17 @@ public class FunctionalCountOccurrences implements CountOccurrences {
     private int findLeft (final int target, final int[] arr) {
         final Range range = new Range(0, arr.length-1);
         while (range.max - range.min > 1) {
-            B.ool(arr[range.mid()] >= target).branch(
+            B.ool(arr[range.mid.execute()] >= target).branch(
                 range.moveMax,
                 range.moveMin
             );
         }
-        return (Integer) B.ool(arr[range.min] == target).branch(
-            new Scalar(range.min),
-            new Function() {
-                public Object execute() {
+        return B.ool(arr[range.min] == target).branch(
+            new Constant(range.min),
+            new Function<Integer>() {
+                public Integer execute() {
                     return B.ool(arr[range.max] == target).branch(
-                        new Scalar(range.max),
+                        new Constant(range.max),
                         negativeOne
                     );
                 }
@@ -55,17 +55,17 @@ public class FunctionalCountOccurrences implements CountOccurrences {
     private int findRight (final int target, final int[] arr) {
         final Range range = new Range(0, arr.length-1);
         while (range.max - range.min > 1) {
-            B.ool(arr[range.mid()] <= target).branch(
+            B.ool(arr[range.mid.execute()] <= target).branch(
                 range.moveMin,
                 range.moveMax
             );
         }
         return (Integer) B.ool(arr[range.max] == target).branch(
-            new Scalar(range.max),
-            new Function() {
-                public Object execute() {
+            new Constant(range.max),
+            new Function<Integer>() {
+                public Integer execute() {
                     return B.ool(arr[range.min] == target).branch(
-                        new Scalar(range.min),
+                        new Constant(range.min),
                         negativeOne
                      );
                 };
@@ -83,58 +83,51 @@ public class FunctionalCountOccurrences implements CountOccurrences {
         int min;
         int max;
         
-        int mid() {return (min + max)/2;}
+        Function<Integer> size = new Function<Integer>() {
+            public Integer execute() {
+                return max - min + 1;
+            }
+        };
         
-        static Function size(final int min, final int max) {
-            return new Function() {
-                public Object execute() {
-                    return max - min + 1;
-                }
-            };
-        }
+        Function<Integer> mid = new Function<Integer>() {
+            public Integer execute() {
+                return (min + max) /2;
+            }
+        };
         
-        Function moveMax = new Function() {
-            public Object execute() {
-                max = mid();
+        Function<Void> moveMax = new Function<Void>() {
+            public Void execute() {
+                max = mid.execute();
                 return null;
             }
         };
-        Function moveMin = new Function() {
-            public Object execute() {
-                min = mid();
+        
+        Function<Void> moveMin = new Function<Void>() {
+            public Void execute() {
+                min = mid.execute();
                 return null;
             }
         };
     }
     
-    static interface Function {
-        public Object execute();
+    static interface Function<T> {
+        public T execute();
     }
     
-    static class Scalar implements Function {
-        final Object val;
-        public Scalar(Object val) {
+    static class Constant<T> implements Function<T> {
+        final T val;
+        public Constant(T val) {
             this.val = val;
         }
-        public Object execute() {
+        public T execute() {
             return val;
-        }
-    }
-    
-    static class Result implements Function {
-        final Function function;
-        public Result(Function function) {
-            this.function = function;
-        }
-        public Object execute() {
-            return function.execute();
         }
     }
     
     static abstract class B {
         private static final B[] bools = new B[]{new FALSE(), new TRUE()};
         
-        abstract Object branch(Function doWhenTrue, Function doWhenFalse);
+        abstract <T> T branch(Function<T> doWhenTrue, Function<T> doWhenFalse);
         
         public static B ool(boolean eval) {
             int boolIndex = getIndex(eval);
@@ -148,14 +141,14 @@ public class FunctionalCountOccurrences implements CountOccurrences {
         
         private static final class TRUE extends B {
             private TRUE() {}
-            Object branch(Function doWhenTrue, Function doWhenFalse) {
+            <T> T branch(Function<T> doWhenTrue, Function<T> doWhenFalse) {
                 return doWhenTrue.execute();
             }
         }
         
         private static final class FALSE extends B {
             private FALSE() {}
-            Object branch(Function doWhenTrue, Function doWhenFalse) {
+            <T> T branch(Function<T> doWhenTrue, Function<T> doWhenFalse) {
                 return doWhenFalse.execute();
             }
         }
